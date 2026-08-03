8247d8da409535e7501bf4af6238bc43bf5bfaa1
rodriguemidbel <rodrigue.midbel@gmail.com>
2026-08-03 22:54:08 +0000

    Update models, Makemigrations & Migrate

--- sigedocBackend/params/models.py
+++ sigedocBackend/params/models.py
@@ -4,118 +4,118 @@ from params.models import *
 # Create your models here.
 class Formulaire(models.Model):
     
-    detail =       models.CharField(max_length=300)
-    repartition =  models.CharField(max_length=300)
-    note =         models.CharField(max_length=300)
+    detail =       models.CharField(max_length=300,blank=True, null=True)
+    repartition =  models.CharField(max_length=300,blank=True, null=True)
+    note =         models.CharField(max_length=300,blank=True, null=True)
     commentaire =  models.TextField(blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-            return f"{self.detail}, {self.commentaire}"
+        return f"{self.detail}, {self.commentaire}"
 
 class DiplomeMaster(models.Model):
     
-    detail =       models.CharField(max_length=500)
-    repartition =  models.IntegerField()
-    note =         models.IntegerField()
+    detail =       models.CharField(max_length=500,blank=True, null=True)
+    repartition =  models.IntegerField(blank=True, null=True)
+    note =         models.IntegerField(blank=True, null=True)
     commentaire =  models.TextField(blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-            return f"{self.detail}, {self.repartition}"
+        return f"{self.detail}, {self.repartition}"
 
 class NoteMaster(models.Model):
     
-    detail =       models.CharField(max_length=500)
-    repartition =  models.IntegerField()
-    note =         models.IntegerField()
+    detail =       models.CharField(max_length=500,blank=True, null=True)
+    repartition =  models.IntegerField(blank=True, null=True)
+    note =         models.IntegerField(blank=True, null=True)
     commentaire =  models.TextField(blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-            return f"{self.detail}, {self.repartition}"
+        return f"{self.detail}, {self.repartition}"
 
 class AncienneteMaster(models.Model):
     
-    detail =       models.CharField(max_length=500)
-    repartition =  models.IntegerField()
-    note =         models.IntegerField()
+    detail =       models.CharField(max_length=500,blank=True, null=True)
+    repartition =  models.IntegerField(blank=True, null=True)
+    note =         models.IntegerField(blank=True, null=True)
     commentaire =  models.TextField(blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-            return f"{self.detail}, {self.repartition}"
+        return f"{self.detail}, {self.repartition}"
 class DiplomeLicence(models.Model):
     
-    detail =       models.CharField(max_length=500)
-    repartition =  models.IntegerField()
-    note =         models.IntegerField()
+    detail =       models.CharField(max_length=500,blank=True, null=True)
+    repartition =  models.IntegerField(blank=True, null=True)
+    note =         models.IntegerField(blank=True, null=True)
     commentaire =  models.TextField(blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-            return f"{self.detail}, {self.repartition}"
+        return f"{self.detail}, {self.repartition}"
 
 class NoteLicence(models.Model):
     
-    detail =       models.CharField(max_length=500)
-    repartition =  models.IntegerField()
-    note =         models.IntegerField()
+    detail =       models.CharField(max_length=500,blank=True, null=True)
+    repartition =  models.IntegerField(blank=True, null=True)
+    note =         models.IntegerField(blank=True, null=True)
     commentaire =  models.TextField(blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-            return f"{self.detail}, {self.repartition}"
+        return f"{self.detail}, {self.repartition}"
 class Curvitae(models.Model):
     
-    detail =       models.CharField(max_length=300)
-    repartition =  models.CharField(max_length=300)
-    note =         models.IntegerField()
+    detail =       models.CharField(max_length=300,blank=True, null=True)
+    repartition =  models.CharField(max_length=300,blank=True, null=True)
+    note =         models.IntegerField(blank=True, null=True)
     commentaire =  models.TextField(blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-            return f"{self.detail}, {self.commentaire}"
+        return f"{self.detail}, {self.commentaire}"
 
 class Age(models.Model):
     
-    detail =       models.CharField(max_length=300)
-    repartition =  models.CharField(max_length=300)
-    note =         models.IntegerField()
+    detail =       models.CharField(max_length=300,blank=True, null=True)
+    repartition =  models.CharField(max_length=300,blank=True, null=True)
+    note =         models.IntegerField(blank=True, null=True)
     commentaire =  models.TextField(blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-            return f"{self.detail}, {self.commentaire}"
+        return f"{self.detail}, {self.commentaire}"
 
 class Genre(models.Model):
     
-    detail =       models.CharField(max_length=300)
-    repartition =  models.CharField(max_length=300)
-    note =         models.IntegerField()
+    detail =       models.CharField(max_length=300,blank=True, null=True)
+    repartition =  models.CharField(max_length=300,blank=True, null=True)
+    note =         models.IntegerField(blank=True, null=True)
     commentaire =  models.TextField(blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-            return f"{self.detail}, {self.commentaire}"
+        return f"{self.detail}, {self.commentaire}"
 
 class Lettremotiv(models.Model):
     
-    detail =       models.CharField(max_length=300)
-    repartition =  models.CharField(max_length=300)
-    note =         models.IntegerField()
+    detail =       models.CharField(max_length=300,blank=True, null=True)
+    repartition =  models.CharField(max_length=300,blank=True, null=True)
+    note =         models.IntegerField(blank=True, null=True)
     commentaire =  models.TextField(blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-            return f"{self.detail}, {self.commentaire}"
+        return f"{self.detail}, {self.commentaire}"
